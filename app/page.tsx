be5a59db7bfc98f0ce5ada9a408e4eb605f97979
Ashar Khan <asharullah.khan2468@gmail.com>
2024-10-17 10:15:29 -0700

    next.js website

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 function page() {
   return (
-    <div>Home page</div>
+    <>
+    
+
+
+    <div className='flex justify-center gap-7 mt-3'>
+    <p>Mobile phones</p>
+    <p>Cars</p>
+    <p>Motorcycles</p>
+    <p>House</p>
+    <p>Video Audios</p>
+    <p>Tablets</p>
+    <p>Land & Plots</p>
+    </div>
+  
+
+    </>
   )
 }
 
